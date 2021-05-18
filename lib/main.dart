b02cb4c1f5ab96a6c769bc3a419261a601078edc
smalugu-sofi <smalugu@sofi.org>
2021-05-18 16:10:12 -0700

    getting expandable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,10 +27,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   void _incrementCounter() {
-    setState(() {
-    });
+    setState(() {});
   }
 
   @override
@@ -46,12 +45,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'Over the board outdoor chess lessons',
-               style: Theme.of(context).textTheme.headline3,
+              style: Theme.of(context).textTheme.headline3,
             ),
+            ExpandablePanel(
+              header: Text('About me '),
+              collapsed: Text(
+                'collapsed',
+                softWrap: true,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              expanded: Text(
+                  "I started learning chess at 11 years of age and became serious and had a full time coach from 13. My coaches inculcated in me a growth mindset and attacking play. My peak chess performance was reached in about 3 years of professional stint. I played mostly in Andhra Pradesh (combined then), India and I was the state championships in Under-14, Under-15, Under-19 and state official championships. I continued playing semi-professionally after joining college and won several collegiate and University championships. I was the captain for the Andhra university and our team got into top 10. I was also captain for the Gitam college team which won the intercollegiate all 4 years. During this period I also started coaching other students and a kid who I coached won national under-7 championship. After few years I came to the US and played in a few tournaments and with in couple of years I got the NM title."),
+            ),
+            Text('About me: '),
             Text('Contact me for additional details'),
           ],
         ),
