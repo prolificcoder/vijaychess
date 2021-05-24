8ffd5fa4a77058cbbf99798b75e38ebd1dd4030f
Satyajit Malugu <a@b.com>
2021-05-24 15:56:07 -0700

    typo fixes

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ Rating:
             child: Column(
               children: <Widget>[
                 Text(
-                  'Over the board outdoor chess lessons',
+                  'Over the board outdoor chess lessons by National Master',
                   style: Theme.of(context).textTheme.headline5,
                 ),
                 SizedBox(height: 16),
@@ -71,10 +71,14 @@ Rating:
                   header: Text('Classes'),
                   collapsed: Text(''),
                   expanded: Text(
-                      'Get started with one hour group lessons, \$20 per class. Max 8 kids at one time. Timings depend on kids level'),
+                      'Get started with one hour group lessons, \$25 per class. Max 8 kids at one time. Timings depend on kids level'),
                 ),
                 SizedBox(height: 16),
-                Text('Location: 2512 242nd Ave SE, Sammamish'),
+                ExpandablePanel(
+                  header: Text('Location'),
+                  collapsed: Text(''),
+                  expanded: Text('2512 242nd Ave SE, Sammamish'),
+                ),
                 SizedBox(height: 16),
                 ExpandablePanel(
                   header: Text('Why in person?'),
@@ -84,10 +88,11 @@ Serious chess tournaments happen over the board and I would like students to get
 Resources available over the internet for chess improvement are vast and I will help motivated students to supplement class learnings.
 '''),
                 ),
+                SizedBox(height: 16),
                 ExpandablePanel(
                   header: Text('Would you have summer camp or tournaments'),
                   collapsed: Text(''),
-                  expanded: Text('Stay tuned... and what this space'),
+                  expanded: Text('Stay tuned... and watch this space'),
                 ),
                 SizedBox(height: 16),
                 ExpandablePanel(
@@ -95,8 +100,9 @@ Resources available over the internet for chess improvement are vast and I will 
                   collapsed: Text(''),
                   expanded: Text('''
 I started learning chess at 11 years of age and became serious and had a full time coach from 13. My coaches inculcated in me a growth mindset and attacking play. 
-My peak chess performance was reached in about 3 years of professional stint. I played mostly in Andhra Pradesh (combined then), India and I was the state championships in Under-14, Under-15, Under-19 and state official championships. 
-I continued playing semi-professionally after joining college and won several collegiate and University championships. I was the captain for the Andhra university and our team got into top 10. I was also captain for the Gitam college team which won the intercollegiate all 4 years. During this period I also started coaching other students and a kid who I coached won national under-7 championship. 
+My peak chess performance was reached in about 3 years of professional stint. I played mostly in Andhra, India and I won the state championships in Under-14, Under-15, Under-19 and state official. 
+I continued playing semi-professionally after joining college and won several collegiate and University championships. I was the captain for the Andhra university where our team got into top 10 in nationals. 
+I was also captain for the Gitam college team which won the intercollegiate all 4 years. During this period I also started coaching other students and a kid who I coached won national under-7 championship. 
 After few years I came to the US and played in a few tournaments and with in couple of years I got the NM title
 '''),
                 ),
@@ -105,7 +111,13 @@ After few years I came to the US and played in a few tournaments and with in cou
                   header: Text('Coaching style'),
                   collapsed: Text(''),
                   expanded: Text(
-                      'While I coach at various levels, my skill and passion shines for committed players who want to compete in tournaments. My focus in teaching will be on practical OTB play with analysis of previous games and retrospection. I teach skills that avoid making blunders and improvement from each serious tournament game. I specialize in middle game and attacking style because I believe this brings rapid development. My coach has this favorite quote, "even if the opponent is a world champion, he has the same set of pieces and has to work with the same rules to defeat you". You are in control of your game, its a great equalizer. Currently my focus is on teaching my young son and hence my rates are very low because he accompanies me during the class. A quote from a 12 yr old student\'s parent "Thanks to your coaching, he has been showing more interest in chess than anything else so far'),
+'''
+While I coach at various levels, my skill and passion shines for committed players who want to compete in tournaments.
+My focus in teaching will be on practical OTB play with analysis of previous games and retrospection. I teach skills that avoid making blunders and improvement from each serious tournament game. 
+I specialize in middle game and attacking style because I believe this brings rapid development. My coach has this favorite quote, "even if the opponent is a world champion, he has the same set of pieces and has to work with the same rules to defeat you".
+You are in control of your game, its a great equalizer. Currently my focus is on teaching my young son and hence my rates are very low because he accompanies me during the class. 
+A quote from a 12 yr old student\'s parent "Thanks to your coaching, he has been showing more interest in chess than anything else so far
+'''),
                 ),
               ],
             ),
@@ -118,7 +130,8 @@ After few years I came to the US and played in a few tournaments and with in cou
         child: Icon(Icons.mail_outlined),
       ),
       persistentFooterButtons: <Widget>[
-        Text('Made with flutter, code hosted at https://github.com/prolificcoder/vijaychess'),
+        Text(
+            'Made with flutter, code hosted at https://github.com/prolificcoder/vijaychess'),
       ],
     );
   }
