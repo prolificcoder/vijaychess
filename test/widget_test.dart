c467f6a18ab9630fd2d4762f0d6280433261846e
Satyajit Malugu <a@b.com>
2021-06-19 14:02:15 -0700

    wip to add firestore mocks

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:vijaychess/main.dart';
 
 void main() {
   testWidgets('Static data test', (WidgetTester tester) async {
+    final firestore = FakeFirebaseFirestore();
+    await firestore.collection('VCC-July').add({
+      'first_name': 'Sankalp',
+    });
     // Build our app and trigger a frame.
     await tester.pumpWidget(VCCApp());
+    await tester.idle();
+    await tester.pump();
 
     expect(find.text('Vijay chess club'), findsOneWidget);
     expect(find.text('About'), findsOneWidget);
