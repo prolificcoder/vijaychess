7df978d16b03bc6735af56ec19bd3f1443181ae8
satya <satyaATmalugu.com>
2021-12-23 21:37:25 -0800

    basic setup not working yet

--- lib/players_screen.dart
+++ lib/players_screen.dart
@@ -1,44 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/firestore.dart';
 
-class PlayersScreen extends StatelessWidget {
+class PlayersScreen extends StatefulWidget {
   PlayersScreen({Key? key}) : super(key: key);
 
+  @override
+  _PlayersScreenState createState() => _PlayersScreenState();
+}
+
+class _PlayersScreenState extends State<PlayersScreen> {
   final _formKey = GlobalKey<FormState>();
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializedFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializedFlutterFire();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      return ErrorWidget(_error);
+    }
+    if (!_initialized) {
+      return CircularProgressIndicator();
+    }
+    final players = FirebaseFirestore.instance.collection('VCC-July');
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Players'),
-      ),
-      body: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data')),
-                    );
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ],
-          )),
-    );
+        appBar: AppBar(
+          title: Text('Players'),
+        ),
+        body: FirestoreListView<Map<String, dynamic>>(
+          query: players,
+          itemBuilder: (context, snapshot) {
+            Map<String, dynamic> user = snapshot.data();
+
+            return Text('User name is ${user['name']}');
+          },
+        ));
   }
 }
