4005785e1e36481fcc5520463ce6ed1d24e84277
satya <satyaATmalugu.com>
2021-12-24 22:57:13 -0800

    fix lint

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:vijaychess/landing_page.dart';
-import 'package:vijaychess/main.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
