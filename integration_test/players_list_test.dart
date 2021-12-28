15c1f511f1b23f4f80e2737465e0c38dd5b151c3
satya <satyaATmalugu.com>
2021-12-28 13:54:49 -0800

    making test work

--- integration_test/players_list_test.dart
+++ integration_test/players_list_test.dart
@@ -5,14 +5,15 @@ import 'package:vijaychess/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  testWidgets("failing test example", (WidgetTester tester) async {
+  testWidgets("Verify home page loads and shows About",
+      (WidgetTester tester) async {
     app.main();
 
     //For web runs Sleep for 2 seconds!!
     //await Future.delayed(const Duration(seconds: 2), () {});
     await tester.pumpAndSettle();
 
-    expect(find.text('Vijay Chess lub'), findsOneWidget);
+    expect(find.text('Vijay Chess Club'), findsOneWidget);
 
     await tester.tap(find.text('About'));
   });
